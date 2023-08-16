86c5684137b3ea82a6b0179131541a41329469d9
Robert <rlgallegos85@gmail.com>
2023-08-15 20:44:42 -0400

    Finished setupd

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+        colors: {
+            primary: "#FFFFFF",
+            secondary: "#000000",
+            tertiary: "#A2B8CF"
+        }
+    },
+  },
+  plugins: [],
+}
+
